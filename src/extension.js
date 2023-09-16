d03c470c172113c4ab46a8739e530b829dd8c2fc
Asmir Belkic <asmir.belkic@gmail.com>
2023-09-16 16:52:51 +0200

    Update

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,10 @@
 const vscode = require("vscode");
 const openurl = require("openurl");
-const messages = require("./messages"); // Import the messages file
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const msg = require("./messages").messages;
+const uuid = require("uuid");
 
 /**
  * Check if a web page exists.
@@ -53,17 +56,17 @@ const showError = (message) => {
  * @param {string} url - The URL to open.
  */
 const openUrl = async (url) => {
-	const isSandboxRunning = await checkIfSandboxIsRunning(messages.urlPrefix);
+	const isSandboxRunning = await checkIfSandboxIsRunning(msg.urlPrefix);
 
 	if (!isSandboxRunning) {
-		showError(messages.noSandboxRunning);
+		showError(msg.noSandboxRunning);
 		return;
 	}
 
 	const doesPageExist = await checkIfPageExists(url);
 
 	if (!doesPageExist) {
-		showError(messages.pageNotFound);
+		showError(msg.pageNotFound);
 		return;
 	}
 
@@ -77,21 +80,21 @@ const openUrl = async (url) => {
 const registerCommand = (context) => {
 	let disposable = vscode.commands.registerCommand("open-with-url-prefix.open", (fileUri) => {
 		if (!fileUri) {
-			showError(messages.noFileSelected);
+			showError(msg.noFileSelected);
 			return;
 		}
 
 		const workspaceFolder = vscode.workspace.getWorkspaceFolder(fileUri);
 
 		if (!workspaceFolder) {
-			showError(messages.noWorkspaceFolder);
+			showError(msg.noWorkspaceFolder);
 			return;
 		}
 
 		const fileRelativePath = vscode.workspace.asRelativePath(fileUri, false);
 		const file = fileRelativePath.replace(/(^|.*\/)(app-sandbox|app)\//, "");
 
-		const url = `${messages.urlPrefix}${file}`;
+		const url = `${msg.urlPrefix}${file}`;
 
 		openUrl(url);
 	});
@@ -104,15 +107,160 @@ const registerCommand = (context) => {
  * @param {vscode.ExtensionContext} context
  */
 const activate = (context) => {
+	const appDir = path.dirname(require.main.filename);
+	const base = path.join(appDir, "vs", "code");
+	const htmlFile = path.join(base, "electron-sandbox", "workbench", "workbench.html");
+	const BackupFilePath = (uuid) => path.join(base, "electron-sandbox", "workbench", `workbench.${uuid}.bak-custom-css`);
+
+	// ####  main commands ######################################################
+
+	async function cmdInstall() {
+		const uuidSession = uuid.v4();
+		await createBackup(uuidSession);
+		await performPatch(uuidSession);
+	}
+
+	async function cmdReinstall() {
+		await uninstallImpl();
+		await cmdInstall();
+	}
+
+	async function cmdUninstall() {
+		await uninstallImpl();
+		disabledRestart();
+	}
+
+	async function uninstallImpl() {
+		const backupUuid = await getBackupUuid(htmlFile);
+		if (!backupUuid) return;
+		const backupPath = BackupFilePath(backupUuid);
+		await restoreBackup(backupPath);
+		await deleteBackupFiles();
+	}
+
+	// #### Backup ################################################################
+
+	async function getBackupUuid(htmlFilePath) {
+		try {
+			const htmlContent = await fs.promises.readFile(htmlFilePath, "utf-8");
+			const m = htmlContent.match(/<!-- !! VSCODE-CUSTOM-CSS-SESSION-ID ([0-9a-fA-F-]+) !! -->/);
+			if (!m) return null;
+			else return m[1];
+		} catch (e) {
+			vscode.window.showInformationMessage(msg.somethingWrong + e);
+			throw e;
+		}
+	}
+
+	async function createBackup(uuidSession) {
+		try {
+			let html = await fs.promises.readFile(htmlFile, "utf-8");
+			html = clearExistingPatches(html);
+			await fs.promises.writeFile(BackupFilePath(uuidSession), html, "utf-8");
+		} catch (e) {
+			vscode.window.showInformationMessage(msg.admin);
+			throw e;
+		}
+	}
+
+	async function restoreBackup(backupFilePath) {
+		try {
+			if (fs.existsSync(backupFilePath)) {
+				await fs.promises.unlink(htmlFile);
+				await fs.promises.copyFile(backupFilePath, htmlFile);
+			}
+		} catch (e) {
+			vscode.window.showInformationMessage(msg.admin);
+			throw e;
+		}
+	}
+
+	async function deleteBackupFiles() {
+		const htmlDir = path.dirname(htmlFile);
+		const htmlDirItems = await fs.promises.readdir(htmlDir);
+		for (const item of htmlDirItems) {
+			if (item.endsWith(".bak-custom-css")) {
+				await fs.promises.unlink(path.join(htmlDir, item));
+			}
+		}
+	}
+
+	// #### Patching ##############################################################
+
+	async function performPatch(uuidSession) {
+		let html = await fs.promises.readFile(htmlFile, "utf-8");
+		html = clearExistingPatches(html);
+
+		const injectHTML = await patchHtml();
+		html = html.replace(/<meta\s+http-equiv="Content-Security-Policy"[\s\S]*?\/>/, "");
+
+		html = html.replace(/(<\/html>)/, `<!-- !! VSCODE-CUSTOM-CSS-SESSION-ID ${uuidSession} !! -->\n` + "<!-- !! VSCODE-CUSTOM-CSS-START !! -->\n" + injectHTML + "<!-- !! VSCODE-CUSTOM-CSS-END !! -->\n</html>");
+		try {
+			await fs.promises.writeFile(htmlFile, html, "utf-8");
+		} catch (e) {
+			vscode.window.showInformationMessage("Admin");
+			disabledRestart();
+		}
+		enabledRestart();
+	}
+	function clearExistingPatches(html) {
+		html = html.replace(/<!-- !! VSCODE-CUSTOM-CSS-START !! -->[\s\S]*?<!-- !! VSCODE-CUSTOM-CSS-END !! -->\n*/, "");
+		html = html.replace(/<!-- !! VSCODE-CUSTOM-CSS-SESSION-ID [\w-]+ !! -->\n*/g, "");
+		return html;
+	}
+
+	async function patchHtml() {
+		let res = "";
+		const imp = await patchHtmlForItem();
+		if (imp) res += imp;
+		console.log(res, imp);
+		return res;
+	}
+	async function patchHtmlForItem() {
+		try {
+			return `<style>
+				.editor-instance .margin-view-overlays .current-line {
+					border-left: 4px solid #50fa7b;
+					background: rgb(116, 207, 136, 0.1);
+				}
+			</style>`;
+		} catch (e) {
+			console.error(e);
+			vscode.window.showWarningMessage("Cannot load");
+			return "";
+		}
+	}
+
+	function reloadWindow() {
+		// reload vscode-window
+		vscode.commands.executeCommand("workbench.action.reloadWindow");
+	}
+	function enabledRestart() {
+		vscode.window.showInformationMessage(msg.enabled, { title: msg.restartIde }).then(reloadWindow);
+	}
+	function disabledRestart() {
+		vscode.window.showInformationMessage(msg.disabled, { title: msg.restartIde }).then(reloadWindow);
+	}
+
+	const installCustomCSS = vscode.commands.registerCommand("extension.installCustomCSS", cmdInstall);
+	const uninstallCustomCSS = vscode.commands.registerCommand("extension.uninstallCustomCSS", cmdUninstall);
+	const updateCustomCSS = vscode.commands.registerCommand("extension.updateCustomCSS", cmdReinstall);
+
+	context.subscriptions.push(installCustomCSS);
+	context.subscriptions.push(uninstallCustomCSS);
+	context.subscriptions.push(updateCustomCSS);
+
+	console.log("vscode-custom-css is active!");
+	console.log("Application directory", appDir);
+	console.log("Main HTML file", htmlFile);
 	registerCommand(context);
 };
 
 /**
  * Deactivate the extension
  */
-const deactivate = () => {};
+exports.activate = activate;
 
-module.exports = {
-	activate,
-	deactivate,
-};
+// this method is called when your extension is deactivated
+function deactivate() {}
+exports.deactivate = deactivate;
